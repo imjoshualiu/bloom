526933369a9964047720de66cf0f8b34fff31709
imjoshualiu <75225570+imjoshualiu@users.noreply.github.com>
2020-12-22 13:15:10 -0800

    update bloom.html and bloom.js

--- bloom.js
+++ bloom.js
@@ -1,5 +1,5 @@
 function random() {
-    var Idx=Math.floor(Math.random()*5)
-    let colors=["bloomRed/bloomRed.html", "bloomOrange/bloomOrange.html", "bloomYellow/bloomYellow.html", "bloomGreen/bloomGreen.html", "bloomBlue/bloomBlue.html", "bloomPurple/bloomPurple.html"]
-    return colors[Idx]
+    var Idx=Math.floor(Math.random()*5);
+    let colors=["bloomRed/bloomRed.html", "bloomOrange/bloomOrange.html", "bloomYellow/bloomYellow.html", "bloomGreen/bloomGreen.html", "bloomBlue/bloomBlue.html", "bloomPurple/bloomPurple.html"];
+    randColor=colors[Idx];
 }
