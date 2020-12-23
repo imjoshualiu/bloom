7e33444bcf521a6097572f52451b75451b82e8c5
imjoshualiu <75225570+imjoshualiu@users.noreply.github.com>
2020-12-23 00:37:09 -0800

    final update

--- bloom.js
+++
@@ -1,7 +0,0 @@
-var function1=document.getElementById("function1");
-function1.onclick = random() {
-    var Idx=Math.floor(Math.random()*5);
-    let colors=["bloomRed/bloomRed.html", "bloomOrange/bloomOrange.html", "bloomYellow/bloomYellow.html", "bloomGreen/bloomGreen.html", "bloomBlue/bloomBlue.html", "bloomPurple/bloomPurple.html"];
-    randColor=colors[Idx];
-    return randColor;
-}
